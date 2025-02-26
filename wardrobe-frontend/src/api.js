00b76774e3eecdbc58713e865a08974d4b1e686e
glenn <glennkamau2@gmail.com>
2025-02-27 02:43:24 +0300

    configure export statement

--- wardrobe-frontend/src/api.js
+++ wardrobe-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: import.meta.env.VITE_API_URL, // Ensure this points to your backend URL
+    baseURL: import.meta.env.VITE_API_URL, 
     withCredentials: true, // Required for cookies
 });
 
