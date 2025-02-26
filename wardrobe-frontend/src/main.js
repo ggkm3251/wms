84978d26ef7ba5080c00b6d708a01c065316d46d
glenn <glennkamau2@gmail.com>
2025-02-26 22:40:38 +0300

    Import the api.js  so that Axios instance is available globally

--- wardrobe-frontend/src/main.js
+++ wardrobe-frontend/src/main.js
@@ -3,5 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import apiClient from './api'; // Import the Axios instance
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+
+// Make the Axios instance available globally
+app.config.globalProperties.$api = apiClient;
+
+app.use(router).mount('#app');
