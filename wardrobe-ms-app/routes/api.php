b514f394c66e555df603eed6870a2e674200ba43
glenn <glennkamau2@gmail.com>
2025-02-26 22:33:21 +0300

    Expose the CSRF token header in config/cors.php

--- wardrobe-ms-app/routes/api.php
+++ wardrobe-ms-app/routes/api.php
@@ -3,6 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClothingItemController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+
+// LOGIN route
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+    ->middleware('guest')
+    ->name('login');
+
+// REGISTER route
+Route::post('/register', [RegisteredUserController::class, 'store'])
+    ->middleware('guest')
+    ->name('register');
+
 
 Route::middleware(['auth:sanctum'])->group(function () {
     // Clothing Items Routes
@@ -11,6 +24,3 @@ Route::middleware(['auth:sanctum'])->group(function () {
     // Categories Routes
     Route::apiResource('categories', CategoryController::class);
 });
-
-// Include authentication routes
-require __DIR__.'/auth.php';
