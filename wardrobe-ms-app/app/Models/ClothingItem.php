fff8fba5b4f67d5d7301d7bd7b81ebdf73ad85b2
glenn <glennkamau2@gmail.com>
2025-02-26 10:26:43 +0300

    update models

--- wardrobe-ms-app/app/Models/ClothingItem.php
+++ wardrobe-ms-app/app/Models/ClothingItem.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ClothingItem extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'description',
+        'category',
+        'image_url',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
