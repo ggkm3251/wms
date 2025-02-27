83aebb2a123698f3005e3176ec1f94a43e81d690
glenn <glennkamau2@gmail.com>
2025-02-27 23:04:39 +0300

    update categoryseeder

--- wardrobe-ms-app/database/seeders/CategorySeeder.php
+++ wardrobe-ms-app/database/seeders/CategorySeeder.php
@@ -17,8 +17,7 @@ class CategorySeeder extends Seeder
 
         foreach ($categories as $category) {
             Category::firstOrCreate(
-                ['name' => $category['name']], // Check for existing category by name
-                $category
+                ['name' => $category] 
             );
         }
     }
