61ef68efb27ae9663633ceedbcf609f6ebe81a82
glenn <glennkamau2@gmail.com>
2025-02-27 13:38:36 +0300

    Update seeders to avoid duplicate entries

--- wardrobe-ms-app/database/seeders/DatabaseSeeder.php
+++ wardrobe-ms-app/database/seeders/DatabaseSeeder.php
@@ -13,11 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::firstOrCreate(
+            ['email' => 'test@example.com'], // Check for existing user by email
+            [
+                'name' => 'Test User',
+                'password' => bcrypt('password'), // Add a default password
+                'email_verified_at' => now(), // Mark the email as verified
+            ]
+        );
     }
 }
